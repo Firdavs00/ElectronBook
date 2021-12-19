3bf19ede125bc330df507627bf8ab47db80b4457
Firdavs <firdavsvlog@gmail.com>
2021-12-19 23:10:15 +0500

    Initial Commit

---
+++ MyNewProject/MyNewProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNewProjectApp.swift
+//  MyNewProject
+//
+//  Created by Firdavs Boltayev on 19/12/21.
+//
+
+import SwiftUI
+
+@main
+struct MyNewProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
